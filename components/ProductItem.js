1e307b460871bbdb172f1a2baf23b08e024be14f
TONY ODUOR <45414713+22052000@users.noreply.github.com>
2023-04-11 13:41:38 +0300

    Update ProductItem.js

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/Link';
+import Link from 'next/link';
 import React from 'react';
 
 export default function ProductItem({ product, addToCartHandler }) {
